c84a8d733534a8982a17f3c6300f7ba88b7df1df
anishachakradhar <anishachakradhar@gmail.com>
2019-09-28 16:19:55 +0545

    add hacker news

--- src/components/HackerNews.vue
+++ src/components/HackerNews.vue
@@ -2,10 +2,10 @@
   <div class="container">
     <div class="card">
       <div class="heading">
-        <h4 class="title">Netflix Ratings</h4>
+        <h4 class="title">Hacker News</h4>
       </div>
       <div class="search">
-        <input type="text" class="form-control" placeholder="Search by title" />
+        <input v-model="title" type="text" class="form-control" placeholder="Search by title" />
       </div>
       <div class="content">
         <div>
@@ -16,9 +16,11 @@
             <p></p>
           </div>
           <ul class="list">
-            <li>
-              <a href></a>
-              <span>By:</span>
+            <li v-for="(item,index) in news" :key="index">
+              <div v-if="item.title">
+                <a href>{{item.title}}</a>
+                <span>By:{{item.author}}</span>
+              </div>
             </li>
           </ul>
         </div>
@@ -28,8 +30,39 @@
 </template>
 
 <script>
+import axios from "axios";
 export default {
-  name: "HackerNews"
+  name: "HackerNews",
+  data() {
+    return {
+      news: [],
+      title: ""
+    };
+  },
+  async created() {
+    try {
+      const response = await axios.get("http://hn.algolia.com/api/v1/search");
+      this.news = response.data.hits;
+      console.log(this.news);
+    } catch (error) {}
+  },
+  watch: {
+    async title(value) {
+      try {
+        const response = await axios.get(
+          "http://hn.algolia.com/api/v1/search",
+          {
+            params: {
+              query: value
+            }
+          }
+        );
+        this.news = response.data.hits;
+      } catch (error) {
+        console.log(value);
+      }
+    }
+  }
 };
 </script>
 
